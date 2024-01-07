15940591a7122d7b352d271e9bad757d292f3161
Mrinal Deb <mrinal437a@gmail.com>
2024-01-08 03:34:11 +0530

    Replace with an appropriate commit message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer' 
+import Clients from './Components/Clients'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Clients/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
